1ad2581f4a79a46a465090e2f80381d82a877f41
madison <longmadison@gmail.com>
2024-11-11 18:10:34 -0800

    defining text for outline

--- hw/rtl/Vortex_hpdcache_if_adapter.sv
+++ hw/rtl/Vortex_hpdcache_if_adapter.sv
@@ -1,1 +1,9 @@
 // this is a temporary file we can use for the vortex adapter
+
+// in the cva6 repo, this file served the purpose of mapping its core’s request and response signals to the HPDCache
+
+//THINGS WE NEED:
+// * handle input output requests
+// * Atomic operations? (these were included in cva6 and I am not quite sure if they are needed here)
+// * Flush mannagement? (these were included in cva6 and I am not quite sure if they are needed here)
+// * possible bank routing and mannagement 
